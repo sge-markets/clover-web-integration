70f86c14c9f04a80ab28718b90ec0cbf4446eb9d
Amichay Oren <amichay@sgeltd.com>
2022-12-26 14:47:39 -0500

    initial test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # clover-web-integration
+
+[![pdm-managed](https://img.shields.io/badge/pdm-managed-blueviolet)](https://pdm.fming.dev)
---
+++ main.py
@@ -0,0 +1,26 @@
+import aiohttp
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+
+@app.get("/")
+async def oauth_step1(client_id: str, employee_id: str, merchant_id: str, code: str):
+    params = [
+        ("client_id", client_id),
+        ("code", code),
+        ("client_secret", os.environ["CLOVER_SECRET"]),
+    ]
+    print(params)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://sandbox.dev.clover.com/oauth/token", params=params
+        ) as response:
+            print("Status:", response.status)
+            print("Content-type:", response.headers["content-type"])
+
+            html = await response.text()
+            print("Body:", html, "...")
+
+    return {}
